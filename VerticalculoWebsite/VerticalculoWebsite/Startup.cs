7957d7e29f19738dd7cbe67ab60ffd8b418759e2
Ricardo Cruz <ricardoc.94911@gmail.com>
2015-06-28 19:24:18 +0100

    no message

---
+++ VerticalculoWebsite/VerticalculoWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VerticalculoWebsite.Startup))]
+namespace VerticalculoWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
