1a876de90020464edb85bbd61b9d5b9e65e07c41
Ricardo Cruz <ricardoc.94911@gmail.com>
2015-07-21 22:40:22 +0100

    no message

---
+++ TI2 - Prática/VerticalculoWebsite/VerticalculoWebsite/Models/AvencasModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VerticalculoWebsite.Models
+{
+    public class AvencasContext : DbContext
+    {
+        public AvencasContext() : base("DefaultConnection"){
+
+        }
+
+        public DbSet<AvencasEntity> AvencaProfiles { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    [Table("AvencasEntity")]
+    public class AvencasEntity
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int AvencaId { get; set; }
+
+        [ForeignKey("userprofile")]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "A {0} tem de ter pelo menos {2} caracteres.", MinimumLength = 10)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public float Valor { get; set; }
+
+        [Required]
+        public DateTime DataAvenca { get; set; }
+
+        [DefaultValue("Por Pagar")]
+        public string EstadoPagamento { get; set; }
+
+        public DateTime DataPagamento { get; set; }
+
+        [DefaultValue(false)]
+        public bool AvisoPagamento { get; set; }
+
+        public virtual UserProfile userprofile { get; set; }
+    }
+}
